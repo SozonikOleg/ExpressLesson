fe5d90b8b239a552bf4ed450098c31305646b65e
SozonikOleg <sozonik999@gmail.com>
2019-11-16 01:32:10 +0300

    added folder public

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 ﻿var express = require('express');
 var app = express();
 
+let count = 0;
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send(`Hello World! Count ${count += 1}`);
 });
 
-app.listen(3003, function () {
-  console.log('Example app listening on port 3003!');
+app.listen(3000, function () {
+  count
+  console.log('Example app listening on port 3000!');
+});
+
+app.use('/static', express.static('public'));
+
+app.use(function (req, res, next) {
+  res.status(404).send('Sorry cant find that!');
 });
